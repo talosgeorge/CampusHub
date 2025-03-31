3ae5c46f205d5d4b33e3b2ecafb5f4007789bc65
Darius Simonca <simonca.darius48@yahoo.com>
2025-03-31 09:59:44 +0300

    Home-page almost done

--- campushub.client/src/app/app.component.ts
+++ campushub.client/src/app/app.component.ts
@@ -1,16 +1,7 @@
-<<<<<<< Updated upstream
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
-=======
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
->>>>>>> Stashed changes
+
 
 @Component({
   selector: 'app-root',
@@ -18,30 +9,7 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.scss'
 })
-<<<<<<< Updated upstream
-export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.getForecasts();
-  }
-
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
-
-  title = 'campushub.client';
-=======
 export class AppComponent {
  
->>>>>>> Stashed changes
 }

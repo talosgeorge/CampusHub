062086e4f08ecb7e12dafc1ed14be651684297f3
talos-george-bogdan <161320838+talosgeorge@users.noreply.github.com>
2025-05-20 19:11:51 +0300

    Personal Computer Changes

--- CampusHub.Server/obj/Debug/net8.0/CampusHub.Server.AssemblyInfo.cs
+++ CampusHub.Server/obj/Debug/net8.0/CampusHub.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampusHub.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23cfa747c5a74c4a443b731af02e962618434bd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e187ce8503989a52a0089b629bb81fafa5376eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampusHub.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampusHub.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
